e1a224c2dc3a44deb998b218de8224b088ab73b7
Valmir Memeti <valmirmemeti@gmail.com>
2021-09-17 03:49:18 +0200

    more refactoring and renaming

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -23,10 +23,9 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-rootProject.name = "external plugins example"
+rootProject.name = "onetick prayer helper"
 
-include(":kotlinexample")
-include(":javaexample")
+include(":onetickprayerhelper")
 
 for (project in rootProject.children) {
     project.apply {
